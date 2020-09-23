2123664422ad326fea16e3063b4d822a9cab245c
Ben Brumby <bbrumby00@gmail.com>
2020-09-22 20:28:34 -0400

    Fixed cards somewhat. Still need a bit of work

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,6 @@ $(document).ready(function () {
 
 $("#weather-btn").on("click", function(event) {
     event.preventDefault();
-    console.log("clicked!");
 
     var citySearch = $("#weather-input").val().trim();
 
@@ -26,6 +25,11 @@ function currentWeather(cityName) {
         $("current-weather").empty();
         console.log(data);
         var weatherDiv = $("<div>");
+        weatherDiv.addClass("card-body")
+        var weatherCardDiv = $("<div>");
+        weatherCardDiv.addClass("card");
+
+
         var temp =data.main.temp;
         var humidity = data.main.humidity;
         var wind = data.wind;
@@ -33,12 +37,15 @@ function currentWeather(cityName) {
         cityLon = data.coord.lon;
         console.log(cityLon);
         console.log(cityLat);
-        var tempOutput = $("<p>").text("Temperature: " + Math.floor((temp - 273.15) * 9/5 + 32) + "\xB0" + "F");
-        var humOutput = $("<p>").text("Humidity: " + humidity + "%");
-        var windOutput = $("<p>").text("Wind: " + wind.speed + "mph at " + wind.deg + " degrees.");
+        var tempOutput = $("<h6>").text("Temperature: " + Math.floor((temp - 273.15) * 9/5 + 32) + "\xB0" + "F");
+        var humOutput = $("<h6>").text("Humidity: " + humidity + "%");
+        var windOutput = $("<h6>").text("Wind: " + wind.speed + "mph at " + wind.deg + " degrees.");
+        
+
         weatherDiv.append(tempOutput);
         weatherDiv.append(humOutput);
         weatherDiv.append(windOutput);
+    
 
         
 
@@ -53,28 +60,36 @@ function currentWeather(cityName) {
 
     .then(function(forecastData){
         console.log(forecastData);
-        $("#forecast").empty();
+        $("forecast").empty();
 
         for (i=0; i < 5; i++) {
         var forecastDiv = $("<div>");
+        forecastDiv.addClass("card-body");
+        var cardDiv = $("<div>");
+        cardDiv.addClass("card");
         var dt = forecastData.daily[i].dt;
         var forecastTemp =forecastData.daily[i].temp.day;
         var forecastHum = forecastData.daily[i].humidity;
             console.log("time", moment.unix(dt).format("MMM Do YYYY"))
 
-        var forecastDTOut = $("<p>").text("Date: " + moment.unix(dt).format("MMM Do YYYY"));
-        var forecastTempOut = $("<p>").text("Temperature: " + Math.floor((forecastTemp-273.15) * 9/5 +32) + "\xB0" + "F");
-        var forecastHumOut = $("<p>").text("Humidity: " + forecastHum + "%");
+        var forecastDTOut = $("<h6>").text(moment.unix(dt).format("MMM Do YYYY"));
+        var forecastTempOut = $("<h6>").text("Temperature: " + Math.floor((forecastTemp-273.15) * 9/5 +32) + "\xB0" + "F");
+        var forecastHumOut = $("<h6>").text("Humidity: " + forecastHum + "%");
         forecastDiv.append(forecastDTOut);
         forecastDiv.append(forecastTempOut);
         forecastDiv.append(forecastHumOut);
 
-        $("#forecast").append(forecastDiv);
+        $(cardDiv).append(forecastDiv);
+        $("#forecast").append(cardDiv);
         }
 
         var uvi = forecastData.daily[0].uvi;
-        var uviOut = $("<div>").text("UVI: " + uvi);
+        var uviOut = $("<h6>").text("UVI: " + uvi);
+        var cityNameEl = data.name;
+        var cityNameOut =  $("<h3>").text(cityNameEl);
+        weatherDiv.prepend(cityNameOut);
         weatherDiv.append(uviOut);
-        $("#current-weather").prepend(weatherDiv);
+        weatherCardDiv.append(weatherDiv);
+        $("#current-weather").prepend(weatherCardDiv);
 
     })})}})
